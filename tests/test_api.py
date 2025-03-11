a3755c84e38063fb64ef9e0771e917996169d965
Владислав <kant1755@mail.ru>
2025-03-11 19:36:30 +0300

    Tests: change tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.main import app
-from app.task_dao import TaskDAO
 from db.models import Base, Status
 from db.session import get_db
 
@@ -22,69 +21,54 @@ async def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 
-class TestTaskAPI:
+@pytest.mark.asyncio
+async def test_one_test_for_all_endpoint():
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
-    @pytest.mark.asyncio
-    async def test_create_task(self):
-        async with test_engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
-        
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as ac:
+
+        fail_task_data = {"title": 123, "status": Status.PENDING.value}
+        response = await ac.post("tasks/create", json=fail_task_data)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]["msg"] == "Input should be a valid string"
+
         task_data = {"title": "Test Task", "status": Status.PENDING.value}
+        response = await ac.post("tasks/create", json=task_data)
+        assert response.status_code == 201
 
-        async with AsyncClient(
-            transport=ASGITransport(app=app),
-            base_url="http://test",
-        ) as ac:
-            response = await ac.post("tasks/create", json=task_data)
+        response = await ac.get("tasks/")
+        assert len(response.json()) == 1
+        assert response.json()[0].get("title") == "Test Task"
 
-            assert response.status_code == 201
+        fail_update_data = {"status": "in_progress"}
+        response = await ac.patch("tasks/update_status/1", json=fail_update_data)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]["msg"] == "Input should be 'pending' or 'done'"
 
-            response = await ac.get("tasks/")
+        update_data = {"status": Status.DONE.value}
+        response = await ac.patch("tasks/update_status/10", json=update_data)
+        assert response.status_code == 404
 
-            assert response.json()[0].get("id") == 1
+        response = await ac.patch("tasks/update_status/1", json=update_data)
+        assert response.status_code == 200
 
-    @pytest.mark.asyncio
-    async def test_get_tasks(self):
-        async with test_engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
+        response = await ac.get("tasks/")
+        assert response.status_code == 200
+        assert response.json()[0].get("status") == "done"
 
-        async for session in override_get_db():
-            task_dao = TaskDAO(db=session)
-            await task_dao.create_task(title="First task", status=Status.PENDING)
-            await task_dao.create_task(title="Second task", status=Status.PENDING)
-            await session.commit()
-        
-        async with AsyncClient(
-            transport=ASGITransport(app=app),
-            base_url="http://test",
-        ) as ac:
-            response = await ac.get("tasks/")
+        task_data_2 = {"title": "Two Test Task", "status": Status.PENDING.value}
+        response = await ac.post("tasks/create", json=task_data_2)
+        assert response.status_code == 201
 
-            assert len(response.json())
-            assert response.json()[1].get('title') == "Second task"
-            
+        response = await ac.get("tasks/?status=pending")
+        assert response.status_code == 200
+        assert len(response.json()) == 1
 
-    @pytest.mark.asyncio
-    async def test_update_tasks(self):
-        async with test_engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
-
-        async for session in override_get_db():
-            task_dao = TaskDAO(db=session)
-            await task_dao.create_task(title="First task", status=Status.PENDING)
-            await session.commit()
-        
-        async with AsyncClient(
-            transport=ASGITransport(app=app),
-            base_url="http://test",
-        ) as ac:
-            task_data = {"status": Status.DONE.value}
-            response = await ac.patch("tasks/update_status/1", json=task_data)
-
-            response = await ac.get("tasks/")
-
-            assert response.status_code == 200
-            assert response.json()[0].get("status") == "done"
+        response = await ac.get("/tasks/?page=1&size=1")
+        assert response.status_code == 200
+        assert len(response.json()) == 1
